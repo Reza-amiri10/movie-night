0a6551e2afddf60bfbf51f4b0d41612884b5b8d1
Apple <mac@Apples-MacBook-Pro.local>
2025-08-19 11:16:03 +0300

    add to watch list button

--- src/components/RatingStars.js
+++ src/components/RatingStars.js
@@ -8,16 +8,19 @@ export default function RatingStars() {
   const [value, setValue] = useState(1);
 
   return (
-    <Box className="stars-box" sx={{ "& > legend": { mt: 0 } }}>
-      <Rating
-        name="simple-controlled"
-        max={10}
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      />
-      <Typography component="legend">{value}</Typography>
-    </Box>
+    <div className="stars-btn">
+      <Box className="stars-box" sx={{ "& > legend": { mt: 0 } }}>
+        <Rating
+          name="simple-controlled"
+          max={10}
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        />
+        <Typography component="legend">{value}</Typography>
+      </Box>
+      <button className="add-btn">+ Add to watch list</button>
+    </div>
   );
 }
